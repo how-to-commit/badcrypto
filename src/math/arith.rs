ae7ecc779babe40258339b07ee25fb6d15eab69c
JL <52496366+how-to-commit@users.noreply.github.com>
2025-05-24 19:41:08 +0800

    feat(bignum): borrowing sub

--- src/math/arith.rs
+++ src/math/arith.rs
@@ -1,7 +1,41 @@
+/// performs lhs + rhs + carry, returning (result, carry)
+/// carry can be 0, 1, or 2
 #[inline]
 pub(crate) fn carrying_add(lhs: u32, rhs: u32, carry: u32) -> (u32, u32) {
     let (interim_sum, overflow1) = lhs.overflowing_add(rhs);
     let (res_sum, overflow2) = interim_sum.overflowing_add(carry);
     let res_overflow = (overflow1 as u32) + (overflow2 as u32);
     (res_sum, res_overflow)
+}
+
+/// lhs - rhs - borrow, returning (result, borrow)
+///
+///    1 0                     lhs rhs cry  res cry
+/// \-   9    =>   borrowing_sub(0, 9, 0) = (1, 1)
+///  ------        borrowing_sub(1, 0, 1) = (0, 0)
+///    0 1         res = 01
+#[inline]
+pub(crate) fn borrowing_sub(lhs: u32, rhs: u32, borrow: u32) -> (u32, u32) {
+    let (interim_res, borrow1) = lhs.overflowing_sub(rhs);
+    let (res_sum, borrow2) = interim_res.overflowing_sub(borrow);
+    let res_overflow = (borrow1 as u32) + (borrow2 as u32);
+    (res_sum, res_overflow)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(carrying_add(5, 10, 0), (15, 0));
+        assert_eq!(carrying_add(5, 10, 1), (16, 0));
+        assert_eq!(carrying_add(u32::MAX, 10, 1), (10, 1));
+    }
+
+    #[test]
+    fn test_sub() {
+        assert_eq!(borrowing_sub(10, 5, 0), (5, 0));
+        assert_eq!(borrowing_sub(5, 10, 1), (u32::MAX - 5, 1));
+    }
 }
--- src/math/bignum/biguint.rs
+++ src/math/bignum/biguint.rs
@@ -1,4 +1,4 @@
-use crate::math::arith::carrying_add;
+use crate::math::arith::{borrowing_sub, carrying_add};
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct BigUint {
@@ -10,40 +10,53 @@ impl BigUint {
     const NUM_LIMBS: usize = 64;
     const LIMB_SIZE: u32 = u32::MAX;
 
+    pub const fn zero() -> Self {
+        Self {
+            limbs: [0; Self::NUM_LIMBS],
+        }
+    }
+
     pub fn from_slice(val: &[u32]) -> Self {
         if val.len() > Self::NUM_LIMBS {
             panic!("Attempt to create BigUint with overflow.");
         }
 
-        let mut ret = BigUint {
-            limbs: [0; Self::NUM_LIMBS],
-        };
+        let mut ret = Self::zero();
         ret.limbs[0..val.len()].copy_from_slice(&val);
         ret
     }
 
-    pub fn from_u128(val: u128) -> Self {
-        let mut ret = BigUint {
-            limbs: [0; Self::NUM_LIMBS],
-        };
+    pub const fn from_u128(val: u128) -> Self {
+        let mut ret = Self::zero();
         let mut v = val;
 
-        for i in 0..4 {
-            ret.limbs[i] = (v & (u32::MAX as u128)) as u32;
-            v >>= 32;
-        }
+        ret.limbs[0] = (v & (Self::LIMB_SIZE as u128)) as u32;
+        v >>= 32;
+        ret.limbs[1] = (v & (Self::LIMB_SIZE as u128)) as u32;
+        v >>= 32;
+        ret.limbs[2] = (v & (Self::LIMB_SIZE as u128)) as u32;
+        v >>= 32;
+        ret.limbs[3] = (v & (Self::LIMB_SIZE as u128)) as u32;
 
         ret
     }
 
+    /// perform self + rhs + carry, returning self and the carry
     pub fn carrying_add(&self, rhs: &Self, mut carry: u32) -> (Self, u32) {
-        let mut ret = BigUint {
-            limbs: [0; Self::NUM_LIMBS],
-        };
+        let mut ret = Self::zero();
         for i in 0..Self::NUM_LIMBS {
             (ret.limbs[i], carry) = carrying_add(self.limbs[i], rhs.limbs[i], carry);
         }
         (ret, carry)
+    }
+
+    /// perform self - rhs - borrow, returning self and the borrow
+    pub fn borrowing_sub(&self, rhs: &Self, mut borrow: u32) -> (Self, u32) {
+        let mut ret = Self::zero();
+        for i in 0..Self::NUM_LIMBS {
+            (ret.limbs[i], borrow) = borrowing_sub(self.limbs[i], rhs.limbs[i], borrow);
+        }
+        (ret, borrow)
     }
 }
 
@@ -52,8 +65,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_constructor() {
-        assert_eq!(u32::MAX, 0xFFFF_FFFF);
+    fn constructor() {
+        assert_eq!(u32::MAX, 0xFFFF_FFFF); // sanity
 
         let huge_number = 5;
         let fu128 = BigUint::from_u128(huge_number as u128);
@@ -65,9 +78,32 @@ mod tests {
         assert_eq!(big_fu128, big_fslice);
     }
 
-    // #[test]
-    // fn carrying_add_no_carry() {
-    //     // assert_eq!()
-    //     todo!()
-    // }
+    #[test]
+    fn carrying_add_no_carry() {
+        // bigger than one limb size
+        let res1 = BigUint::from_u128(u64::MAX as u128 + 5);
+        let op11 = BigUint::from_u128(u64::MAX as u128);
+        let op12 = BigUint::from_u128(5 as u128);
+        assert_eq!(op11.carrying_add(&op12, 0), (res1, 0));
+
+        // smaller than one limb size
+        let res2 = BigUint::from_u128(15 as u128);
+        let op21 = BigUint::from_u128(10 as u128);
+        let op22 = BigUint::from_u128(5 as u128);
+        assert_eq!(op21.carrying_add(&op22, 0), (res2, 0));
+
+        // cross one limb size
+        let res3 = BigUint::from_u128(0x1_0000_0004);
+        let op31 = BigUint::from_u128(0xFFFF_FFFF);
+        let op32 = BigUint::from_u128(5 as u128);
+        assert_eq!(op31.carrying_add(&op32, 0), (res3, 0));
+    }
+
+    #[test]
+    fn borrowing_sub_no_carry() {
+        let res1 = BigUint::from_u128(u64::MAX as u128 - 5);
+        let op11 = BigUint::from_u128(u64::MAX as u128);
+        let op12 = BigUint::from_u128(5 as u128);
+        assert_eq!(op11.borrowing_sub(&op12, 0), (res1, 0));
+    }
 }
